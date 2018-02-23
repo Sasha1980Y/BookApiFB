2edc89aeb907be3f8ca4732ec26abe06735abf34
Александр Яковенко <alexanderyakovenko@alexanders-macbook-air.local>
2018-02-23 23:12:23 +0200

    async download - add reloadData in block

--- BookApi/BooksViewController/BooksViewController.swift
+++ BookApi/BooksViewController/BooksViewController.swift
@@ -37,7 +37,6 @@ struct JSON: Decodable {
     
 }
 
-
 class BooksViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -46,14 +45,36 @@ class BooksViewController: UIViewController {
     var books = [JSON.Items]()
     var numberSelectBook: Int?
     
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action:
+            #selector(BooksViewController.handleRefresh(_:)),
+                                 for: UIControlEvents.valueChanged)
+        refreshControl.tintColor = UIColor.red
+        
+        return refreshControl
+    }()
+    
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        
+        
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         searchBar.delegate = self as UISearchBarDelegate
+        
+        self.tableView.addSubview(self.refreshControl)
     }
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = true
+        self.tableView.reloadData()
     }
     
     func downloadBooks(bookTitle: String) {
@@ -66,6 +87,7 @@ class BooksViewController: UIViewController {
         
         URLSession.shared.dataTask(with: url) { (data, resp, error) in
             
+            
             print("download books")
             guard let data = data else {
                 return
@@ -73,53 +95,37 @@ class BooksViewController: UIViewController {
             guard error == nil else {
                 return
             }
-            
             do {
-                let json = try JSONDecoder().decode(JSON.self, from: data)
                 
+                let json = try JSONDecoder().decode(JSON.self, from: data)
+                DispatchQueue.main.async {
                 if let countItem = json.items {
                     self.books = countItem
+                    
                 }
-                
-                //
+            
                 DispatchQueue.main.async(execute: { () -> Void in
                     self.tableView.reloadData()
-                    //self.refreshControl?.endRefreshing()
                 })
-                
-                //
-                
-                //DispatchQueue.main.async {
-                  //  self.tableView.reloadData()
-               // }
-                
-                
+                   
+                }
                 print("Books VC")
                 
             } catch let error {
                 print(error)
             }
             }.resume()
-        
-        
-        
-        
-        
     }
     override var prefersStatusBarHidden: Bool {
         return true
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "Detail" {
             let nextVC: DetailViewController = segue.destination as! DetailViewController
             if let number = numberSelectBook {
                 nextVC.book = books[number]
             }
         }
-        
-        
-        
     }
     
 }
@@ -129,8 +135,6 @@ extension BooksViewController: UITableViewDataSource {
         return books.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
         tableView.register(UINib(nibName: "NameBookTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? NameBookTableViewCell
         
@@ -138,7 +142,6 @@ extension BooksViewController: UITableViewDataSource {
             if let titleText = volumeInfo.title {
                 cell?.titleLabel.text = titleText
             }
-            
             var authors = ""
             if let author = volumeInfo.authors {
                 for item in author {
@@ -150,60 +153,70 @@ extension BooksViewController: UITableViewDataSource {
                     
                 }
             }
-            
-            
-            DispatchQueue.global(qos: .userInitiated).async {
+        
             
             if let linkImage = volumeInfo.imageLinks?.smallThumbnail {
                     print(linkImage)
+                
+                
+                //cell?.imageView?.imageFromServerURL(urlString: linkImage)
+                //cell?.imageView?.loadImageUsingCache(withUrl: linkImage)
+                
+                
+// WORK ONLY BLOCK !!!
+                
+                cell?.imageView?.loadImageUsingCache(withUrl: linkImage, block: {
+                    self.tableView.reloadData()
+                })
+                
+                
+                
+                
+                
+                /*
                 
                     if let url = URL(string: linkImage) {
                         do {
                             let data = try Data(contentsOf: url) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
                             DispatchQueue.main.async {
+                            
                             cell?.imageView?.image = UIImage(data: data)
                             print("cellForRowAt")
+                            
+                             self.tableView?.reloadData()
+                            
                             }
+                            //self.tableView.reloadData()
                         } catch {
                             print("image not exist", error)
-                            
                         }
                         
-                    }
+                    }*/
                 
-                    
-                    
-                    
                 }
-                
-                
-                
-                
-            }
-        
+            
+           // }
+            
             
             
             cell?.authorLabel.text = authors
         }
         
-        cell?.preservesSuperviewLayoutMargins = false
-        cell?.separatorInset = UIEdgeInsets.zero
-        cell?.layoutMargins = UIEdgeInsets.zero
-        
+        //self.tableView.layoutSubviews()
+        //self.tableView.layoutIfNeeded()
         
         return cell!
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         numberSelectBook = indexPath.row
-        
         performSegue(withIdentifier: "Detail", sender: nil)
-        //let newViewController = DetailViewController()
-        //newViewController.
-        //self.navigationController?.pushViewController(newViewController, animated: true)
-
-        
-        
     }
+    
+    func reload() {
+        self.tableView.reloadData()
+    }
+    
+    
 }
 
 extension BooksViewController: UITableViewDelegate {
@@ -242,6 +255,74 @@ extension UIImageView {
     func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
         downloadedFrom(url: url, contentMode: mode)
+    }
+}
+extension UIImageView {
+    public func imageFromServerURL(urlString: String) {
+        
+        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+            
+        }).resume()
+    }}
+
+
+let imageCache = NSCache<NSString, AnyObject>()
+
+extension UIImageView {
+    
+    
+    func loadImageUsingCache(withUrl urlString : String, block: @escaping () -> Void) {
+        let url = URL(string: urlString)
+        self.image = nil
+        
+        // check cached image
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        // if not, download image from url
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data!) {
+                    imageCache.setObject(image, forKey: urlString as NSString)
+                    self.image = image
+                    block()
+                }
+            }
+            
+        }).resume()
+    }
+}
+
+final class UITableViewWithReloadCompletion: UITableView {
+    private var reloadDataCompletionBlock: (() -> Void)?
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        reloadDataCompletionBlock?()
+        reloadDataCompletionBlock = nil
+    }
+    
+    
+    func reloadDataWithCompletion(completion: @escaping () -> Void) {
+        reloadDataCompletionBlock = completion
+        super.reloadData()
     }
 }
 
